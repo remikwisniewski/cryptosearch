a4b99f2d72397ed4607857556dd024b35733af62
Remi <rwisni736@gmail.com>
2021-05-25 22:00:20 -0500

    WEB API: installing using axios

--- app/src/App.js
+++ app/src/App.js
@@ -1,7 +1,21 @@
-import react from 'react';
+import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 
+
+
 function App() {
+
+  const [coins, setCoins] = useState([]);
+
+  // useEffect takes in arrow function that gets API url, then then response is the data from the API
+  useEffect(()=> {
+    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+    .then(res =>{
+      setCoins(res.data);
+      console.log(res.data);
+    })
+  })
+
   return (
     <div className="App">
       
--- app/src/Components/Coin.js
+++ app/src/Components/Coin.js
@@ -1,7 +1,6 @@
-import React from 'react';
+import React from 'react'
 
-export default function Main() {
-
+export default function Coin() {
     return (
         <div>
             
