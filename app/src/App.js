f6d38e823e8e0689173c9a3e9ebfe7f3906448aa
Remi <rwisni736@gmail.com>
2021-05-25 22:28:07 -0500

    html template

--- app/src/App.js
+++ app/src/App.js
@@ -5,19 +5,34 @@ import axios from 'axios';
 
 function App() {
 
+
+  // creating state for data
   const [coins, setCoins] = useState([]);
 
-  // useEffect takes in arrow function that gets API url, then then response is the data from the API
+  // useEffect takes in arrow function that gets API url, then then response is the data from the API with an error checker
+  
   useEffect(()=> {
     axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
     .then(res =>{
       setCoins(res.data);
       console.log(res.data);
     })
-  })
+    .catch(error => { console.log(error)});
+  }, []);
 
   return (
     <div className="App">
+      
+      <div className="mainApp">
+          <div className="mainSearch">
+            <h1 className="mainText"> Crypto Currency Search</h1>
+            <form>
+              <input type='text' placeholder='Search' className="searchBTN"/>
+            </form>
+          </div>
+      </div>
+      
+
       
     </div>
   );
